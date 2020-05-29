834f5cde7c09a77b983e810fefe6c19d96cc21d3
ABOUHEGAB Amr <G521761@mph.morpho.com>
2020-05-29 11:48:09 +0200

    Append created video request to videos list

--- index.js
+++ index.js
@@ -22,8 +22,10 @@ document.addEventListener('DOMContentLoaded', function() {
     
         createVedioRequest(formEl)
             .then(response => response.json())
-            .then(data => {
-                console.log(data);
+            .then(videoInfo => {
+                if (videoInfo) {
+                    listOfRequestsEl.innerHTML = setVedioRequesTemplate(videoInfo) + listOfRequestsEl.innerHTML;
+                }
             })
             .catch(err => {
                 console.log(err);
@@ -34,34 +36,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-function setVedioRequesTemplate(vedioInfo) {
+function setVedioRequesTemplate(videoInfo) {
     return (`
         <div class="card mb-3">
             <div class="card-body d-flex justify-content-between flex-row">
             <div class="d-flex flex-column">
-                <h3>${vedioInfo.topic_title}</h3>
-                <p class="text-muted mb-2">${vedioInfo.topic_details}</p>
+                <h3>${videoInfo.topic_title}</h3>
+                <p class="text-muted mb-2">${videoInfo.topic_details}</p>
                 <p class="mb-0 text-muted">
-                <strong>Expected results:</strong> ${vedioInfo.expected_result}
+                <strong>Expected results:</strong> ${videoInfo.expected_result}
                 </p>
             </div>
             <div class="d-flex flex-column text-center">
                 <a class="btn btn-link">🔺</a>
-                <h3>${vedioInfo.votes.ups - vedioInfo.votes.downs}</h3>
+                <h3>${videoInfo.votes.ups - videoInfo.votes.downs}</h3>
                 <a class="btn btn-link">🔻</a>
             </div>
             </div>
             <div class="card-footer d-flex flex-row justify-content-between">
             <div>
-                <span class="text-info">${vedioInfo.status.toUpperCase()}</span>
-                &bullet; added by <strong>${vedioInfo.author_name}</strong> on
-                <strong>${(new Date(vedioInfo.submit_date)).toDateString()}</strong>
+                <span class="text-info">${videoInfo.status.toUpperCase()}</span>
+                &bullet; added by <strong>${videoInfo.author_name}</strong> on
+                <strong>${(new Date(videoInfo.submit_date)).toDateString()}</strong>
             </div>
             <div
                 class="d-flex justify-content-center flex-column 408ml-auto mr-2"
             >
                 <div class="badge badge-success">
-                ${vedioInfo.target_level}
+                ${videoInfo.target_level}
                 </div>
             </div>
             </div>
